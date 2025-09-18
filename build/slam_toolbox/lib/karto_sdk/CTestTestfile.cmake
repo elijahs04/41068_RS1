6c0e40975ac2e294e6a7c1bcb234b24b89e2c387
James <james.c.farrell@student.uts.edu.au>
2025-09-18 21:14:07 +1000

    working with movement

--- build/slam_toolbox/lib/karto_sdk/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/elijahs04/ros2_ws/src/41068_RS1/slam_toolbox/lib/karto_sdk
-# Build directory: /home/elijahs04/ros2_ws/src/41068_RS1/build/slam_toolbox/lib/karto_sdk
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
