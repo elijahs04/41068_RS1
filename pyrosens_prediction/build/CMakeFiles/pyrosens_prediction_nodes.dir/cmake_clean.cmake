c316e45543003c979e877324981c05f1d8595592
Luca Sanchez <luca.jaime.sanchez@gmail.com>
2025-11-03 05:54:58 +0000

    give this a whirl

---
+++ pyrosens_prediction/build/CMakeFiles/pyrosens_prediction_nodes.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pyrosens_prediction_nodes.dir/src/predictionNode.cpp.o"
+  "CMakeFiles/pyrosens_prediction_nodes.dir/src/predictionNode.cpp.o.d"
+  "CMakeFiles/pyrosens_prediction_nodes.dir/src/predictionSim.cpp.o"
+  "CMakeFiles/pyrosens_prediction_nodes.dir/src/predictionSim.cpp.o.d"
+  "libpyrosens_prediction_nodes.a"
+  "libpyrosens_prediction_nodes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pyrosens_prediction_nodes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pyrosens_prediction/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/git/41068_RS1/pyrosens_prediction
+# Build directory: /home/student/git/41068_RS1/pyrosens_prediction/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
