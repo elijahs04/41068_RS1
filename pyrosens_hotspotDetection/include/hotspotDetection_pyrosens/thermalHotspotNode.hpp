db73d69c0b4e95c4270f0327be8e60abe2f1fb12
Connor-Lindsell <connor.lindsell@student.uts.edu.au>
2025-11-04 15:39:13 +1100

    Very basic Visulisation of Heatspot need to detect features rather hottest point

--- pyrosens_hotspotDetection/include/hotspotDetection_pyrosens/thermalHotspotNode.hpp
+++ pyrosens_hotspotDetection/include/hotspotDetection_pyrosens/thermalHotspotNode.hpp
@@ -6,6 +6,7 @@
 // ROS 2
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/image_encodings.hpp>
 
 // Project
 #include "hotspotDetection_pyrosens/hotspotDetector.hpp"
@@ -19,19 +20,22 @@ private:
   // ROS callback
   void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr& img_msg);
 
+  // ROS I/O
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr thermalImage_sub_;
+  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr thermalOverlay_pub_;
+
+  // Algorithm
+  thermdetect::HotspotDetector detector_;
+
   // Params and state
   std::string thermal_topic_ = "/thermal_camera/image";
   std::string frame_id_ = "thermal_camera_frame";
   double temp_gain_ = 1.0;
   double temp_offset_ = 0.0;
+  bool publish_overlay_ = true;
 
   // Cached geometry (optional convenience)
   int img_width_ = 0;
   int img_height_ = 0;
 
-  // ROS I/O
-  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr thermalImage_sub_;
-
-  // Algorithm
-  thermdetect::HotspotDetector detector_;
 };
